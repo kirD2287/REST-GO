174bff2d0a0ac02dbd426fd8e6ceeb55ec1d4c84
Kirill Dyachkov <dj_pro98@mail.ru>
2024-08-30 21:22:35 +0600

    auth modifided

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -10,7 +10,7 @@ import (
 func (h *Handler) signUp(c *gin.Context) {
 	var input todo.User
 	if err := c.BindJSON(&input); err!= nil {
-        newErrorResponse(c, http.StatusBadRequest, err.Error())
+        newErrorResponse(c, http.StatusBadRequest, "Invalid input body")
 		return
     }
 
